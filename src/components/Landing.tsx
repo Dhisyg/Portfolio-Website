fa25caff9448304149de56d8bae6903aaef11314
Rishi kumar <ankitkann1133@gmail.com>
2026-04-09 15:25:53 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              MONCY
+              RISHI
               <br />
-              <span>YOHANNAN</span>
+              <span>KUMAR</span>
             </h1>
           </div>
           <div className="landing-info">
